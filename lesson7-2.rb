c2d8053a9226cfa5cb717c9fc9f7efc5b9c611c9
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-03 06:08:47 +0000

    [Finish]2-7/確認問題1/2

---
+++ lesson7-2.rb
@@ -0,0 +1,21 @@
+puts "好きな回数計算できます"
+puts "何回計算しますか？"
+
+input = gets.to_i
+i = 1
+while i <= input do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  
+  puts "計算します"
+  puts "a+b=#{a+b}"
+  puts "a-b=#{a-b}"
+  puts "a*b=#{a*b}"
+  puts "a/b=#{a/b}"
+  
+  i += 1
+end
+
+
