9322d00ebbd76db3caf8092abc58defd3c3bb688
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-10 22:17:42 +0000

    [Finish]

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,21 +1,19 @@
 puts "好きな回数計算できます"
 puts "何回計算しますか？"
 
-input = gets.to_i
+input = gets.to_i #からの変数inputに　入力した物を数字に変えたのをだいにゅう
 i = 1
 while i <= input do
   puts "#{i}回目の計算"
   puts "2つの値を入力してください"
   a = gets.to_i
   b = gets.to_i
-  
+
   puts "計算します"
   puts "a+b=#{a+b}"
   puts "a-b=#{a-b}"
   puts "a*b=#{a*b}"
   puts "a/b=#{a/b}"
-  
+
   i += 1
 end
-
-
